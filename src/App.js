3bef0956640ca7b6323cefa4184e683f847910d7
Md. Abdullah <abdullahkafi996@gmail.com>
2021-03-08 21:08:20 +0600

    add API and get title, description on News component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import Button from '@material-ui/core/Button';
 
 import Container from '@material-ui/core/Container';
 import News from './components/News/News';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b093438169e44148aa7051053a7a930e';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  }, [])
+
   return (
     <div  >
       <Container>
-        <News></News>
-        <Button variant="contained" color="primary">
-          Hello World
-    </Button>
+        <h2>Total News: {articles.length}</h2>
+        {
+          articles.map(article => <News article={article}></News>)
+        }
       </Container>
     </div>
   );
