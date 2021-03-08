1cb899be8be9f51b6af2648e748b9e8a60bdb9eb
Md. Abdullah <abdullahkafi996@gmail.com>
2021-03-08 20:41:47 +0600

    add material UI card component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 
 import Container from '@material-ui/core/Container';
+import News from './components/News/News';
 
 
 function App() {
   return (
     <div  >
       <Container>
+        <News></News>
         <Button variant="contained" color="primary">
           Hello World
     </Button>
