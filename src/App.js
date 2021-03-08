2c673d516ae314d269c36f24f092b0d6e4e00b4e
Md. Abdullah <abdullahkafi996@gmail.com>
2021-03-08 21:52:20 +0600

    add axios to this porject

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import News from './components/News/News';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 function App() {
 
   const [articles, setArticles] = useState([]);
-  useEffect(() => {
+  // useEffect(() => {
 
+  //   const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b093438169e44148aa7051053a7a930e';
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => setArticles(data.articles))
+  // }, [])
+
+  useEffect (() =>{
     const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b093438169e44148aa7051053a7a930e';
-    fetch(url)
-    .then(res => res.json())
-    .then(data => setArticles(data.articles))
+
+    axios(url)
+    .then(data => setArticles(data.data.articles))
   }, [])
 
   return (
